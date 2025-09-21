e87681c32af2e32c2256d78c5f10c6089d579571
Dolev Ben Shushan <dolev532@gmail.com>
2025-09-21 23:46:52 +0300

    with redirect

--- src/main/java/com/handson/tinyurl/controller/AppController.java
+++ src/main/java/com/handson/tinyurl/controller/AppController.java
@@ -1,16 +1,32 @@
 package com.handson.tinyurl.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.handson.tinyurl.model.NewTinyRequest;
 import com.handson.tinyurl.service.Redis;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.cache.CacheProperties;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Random;
 
 @RestController
 public class AppController {
 
+    private static final int MAX_RETRIES = 4;
+    private static final int TINY_LENGTH = 6;
+    Random random = new Random();
+
     @Autowired
     Redis redis;
 
+    @Autowired
+    ObjectMapper om;
+    @Value("${base.url}")
+
+    String baseUrl;
     @RequestMapping(value = "/getkey", method = RequestMethod.GET)
     public String GetKay(@RequestParam String key) {
         return redis.get(key).toString();
@@ -19,6 +35,39 @@ public class AppController {
     @RequestMapping(value = "/setkey", method = RequestMethod.GET)
     public Boolean SetKeyVar(@RequestParam String key , @RequestParam String var) {
         return redis.set(key,var);
+    }
+
+
+    @RequestMapping(value = "/tiny", method = RequestMethod.POST)
+    public String generate(@RequestBody NewTinyRequest request) throws JsonProcessingException {
+        String tinyCode = generateTinyCode();
+        int i = 0;
+        while (!redis.set(tinyCode, om.writeValueAsString(request)) && i < MAX_RETRIES) {
+            tinyCode = generateTinyCode();
+            i++;
+        }
+        if (i == MAX_RETRIES) throw new RuntimeException("SPACE IS FULL");
+        return baseUrl + tinyCode + "/";
+    }
+
+    @RequestMapping(value = "/{tiny}/", method = RequestMethod.GET)
+    public ModelAndView getTiny(@PathVariable String tiny) throws JsonProcessingException {
+        System.out.println("getRequest for tiny: " + tiny);
+        Object tinyRequestStr = redis.get(tiny);
+        NewTinyRequest tinyRequest = om.readValue(tinyRequestStr.toString(),NewTinyRequest.class);
+        if (tinyRequest.getLongUrl() != null) {
+            return new ModelAndView("redirect:" + tinyRequest.getLongUrl());
+        } else {
+            throw new RuntimeException(tiny + " not found");
+        }
+    }
+    private String generateTinyCode() {
+        String charPool = "ABCDEFHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < TINY_LENGTH; i++) {
+            res.append(charPool.charAt(random.nextInt(charPool.length())));
+        }
+        return res.toString();
     }
 
 }
